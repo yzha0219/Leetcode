72988171d54876407ff81c5590ca93aaceb8245c
John Doe <johndoe@hp.com>
2020-07-23 15:19:25 +0800

    Leetcode No.12 Completed

--- src/No_12/Solution1.java
+++ src/No_12/Solution1.java
@@ -3,10 +3,23 @@ package No_12;
 public class Solution1 {
 
     public static void main(String[] args) {
-
+        int num = 1994;
+        System.out.println(intToRoman(num));
     }
 
     public static String intToRoman(int num) {
 
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0;i < values.length && num > 0;i++){
+            if(values[i] <= num){
+               num -= values[i];
+               stringBuilder.append(symbols[i]);
+            }
+        }
+
+        return stringBuilder.toString();
     }
 }
